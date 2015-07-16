a2f70313485f0321e979f23b330f98860c50a5de
Morgan Wigmanich <mwigmanich@patientslikeme.com>
2015-07-16 15:58:53 -0400

    move channel override to router middleware

--- routes.js
+++ routes.js
@@ -2,37 +2,37 @@ var express = require('express');
 var router = express.Router();
 var flip = require('flip');
 var request = require('request');
+var util = require('./util');
 var tokens = process.env.SLACK_TOKENS;
 var webhook = process.env.SLACK_WEBHOOK_URL;
 
-var getChannel = function (body) {
-  if (body.channel_name === 'directmessage' || body.channel_name === 'privategroup') {
-    return body.channel_id;
-  }
-  return '#' + body.channel_name;
-};
-
 // authenticate requests
 router.use(function (req, res, next) {
-
   if (req.body.token !== tokens) {
     return res.status(401).send({ success: false, error: 'Invalid token.' });
   }
-
   next();
-
 });
 
+// set channel override
+router.use(function (req, res, next) {
+  if (req.body.channel_name === 'directmessage' || req.body.channel_name === 'privategroup') {
+    req.channel = req.body.channel_id;
+  } else {
+    req.channel = '#' + req.body.channel_name;
+  }
+  next();
+});
+
+// tableflip command
 router.post('/tableflip', function (req, res) {
 
-  var channel, text, payload, requestOpts;
-
-  channel = getChannel(req.body);
+  var text, payload, requestOpts;
 
   text = req.body.text ? flip(req.body.text) : '┻━┻';
 
   payload = {
-    channel: channel,
+    channel: req.channel,
     text: '(╯°□°）╯︵ ' + text,
     icon_emoji: ':rage1:'
   };
