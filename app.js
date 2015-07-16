d6cf3dfe31e4831b525e2119d12c4ca2718541a7
Morgan Wigmanich <mwigmanich@patientslikeme.com>
2015-07-16 09:52:36 -0400

    add tableflip command

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var app = express();
 // configuration
 app.set('env', process.env.NODE_ENV);
 app.set('port', process.env.PORT || 9000);
+app.set('tokens', process.env.SLACK_TOKENS);
+app.set('webhook', process.env.SLACK_WEBHOOK_URL);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -14,6 +16,45 @@ app.get('/ping', function (req, res) {
   res.json({response: 'PONG'});
 });
 
+var getChannel = function (body) {
+  if (body.channel_name === 'directmessage' || body.channel_name === 'privategroup') {
+    return body.channel_id;
+  }
+  return '#' + body.channel_name;
+};
+
+app.post('/tableflip', function (req, res) {
+
+  var channel, text, payload, requestOpts;
+
+  if (req.body.token !== app.set('tokens')) {
+    return res.status(401).send({ success: false, error: 'Invalid token.' });
+  }
+
+  channel = getChannel(req.body);
+
+  text = req.body.text ? flip(req.body.text) : '┻━┻';
+
+  payload = {
+    channel: channel,
+    text: '(╯°□°）╯︵ ' + text,
+    icon_emoji: ':rage1:'
+  };
+
+  requestOpts = {
+    url: app.get('webhook'),
+    form: { payload: JSON.stringify(payload) }
+  };
+
+  request.post(requestOpts, function (err, resp, body) {
+    if (err) {
+      return res.status(500).send({ success: false, error: err.message });
+    }
+    console.log('Slack response: ' + body);
+  });
+
+});
+
 var server = app.listen(app.get('port'), function () {
-  console.log('App listening at http://%s:%s', server.address().address, server.address().port);
+  console.log('\nApp listening on port %s\n', server.address().port);
 });
