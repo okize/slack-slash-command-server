356a39bd3ce3594e7ec755764f8c6e1c11faf4fd
Morgan Wigmanich <mwigmanich@patientslikeme.com>
2015-09-29 12:05:26 -0400

    add zingbot slash command

---
+++ routes/zingbot.js
@@ -0,0 +1,31 @@
+var request = require('request');
+
+module.exports = function (app) {
+
+  app.post('/zingbot', function (req, res) {
+
+    var imageUrl, payload, requestOpts;
+
+    imageUrl = 'https://pbs.twimg.com/media/CKE98yIUMAA4Y0o.jpg';
+
+    payload = {
+      channel: req.channel,
+      text: imageUrl,
+      icon_emoji: ':frowning:'
+    };
+
+    requestOpts = {
+      url: app.get('webhook'),
+      form: {payload: JSON.stringify(payload)}
+    };
+
+    request.post(requestOpts, function (err, resp, body) {
+      if (err) {
+        return res.status(500).send({success: false, error: err.message});
+      }
+      return res.status(200).send();
+    });
+
+  });
+
+};
