b684d233eee2c1e7f5e1c58ed925c17a21c98a7a
Morgan Wigmanich <mwigmanich@patientslikeme.com>
2015-10-29 07:46:30 -0400

    add rekt slash command

---
+++ routes/rekt.js
@@ -0,0 +1,31 @@
+var request = require('request');
+
+module.exports = function (app) {
+
+  app.post('/rekt', function (req, res) {
+
+    var text, payload, requestOpts;
+
+    text = '\n:white_medium_square: Not rekt  \s\s :ballot_box_with_check: Rekt';
+
+    payload = {
+      channel: req.channel,
+      text: text,
+      icon_emoji: ':grimacing:'
+    };
+
+    requestOpts = {
+      url: app.get('webhook'),
+      form: {payload: JSON.stringify(payload)}
+    };
+
+    request.post(requestOpts, function (err, resp, body) {
+      if (err) {
+        return res.status(500).send({success: false, error: err.message});
+      }
+      return res.status(200).send();
+    });
+
+  });
+
+};
