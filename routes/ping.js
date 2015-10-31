9c97defe32b9c4367718767750e6ee27fd3b1e96
Morgan Wigmanich <mwigmanich@patientslikeme.com>
2015-10-31 07:00:00 -0400

    add ping route

---
+++ routes/ping.js
@@ -0,0 +1,31 @@
+var request = require('request');
+
+module.exports = function (app) {
+
+  app.post('/ping', function (req, res) {
+
+    var text, payload, requestOpts;
+
+    text = 'PONG';
+
+    payload = {
+      channel: req.channel,
+      text: text,
+      icon_emoji: ':grimacing:'
+    };
+
+    requestOpts = {
+      url: app.get('webhook'),
+      form: {payload: JSON.stringify(payload)}
+    };
+
+    request.post(requestOpts, function (err, resp, body) {
+      if (err) {
+        return res.status(500).send({success: false, error: err.message});
+      }
+      return res.status(200).send();
+    });
+
+  });
+
+};
