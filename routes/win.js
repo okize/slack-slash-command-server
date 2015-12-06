42fc12ace7014f278ab4e01741ef71eceaf00d2b
Morgan Wigmanich <mwigmanich@patientslikeme.com>
2015-12-06 07:00:00 -0400

    fix more eslint errors & warnings

--- routes/win.js
+++ routes/win.js
@@ -1,37 +1,28 @@
-var request = require('request');
+const request = require('request');
 
-module.exports = function (app) {
-
-  app.post('/win', function (req, res) {
-
-    var flubrUrl = 'http://flubr.herokuapp.com/api/images/random/pass';
-
-    request(flubrUrl, function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-
-        var payload, requestOpts;
-
-        payload = {
+module.exports = (app) => {
+  app.post('/win', (req, res) => {
+    const flubrUrl = 'http://flubr.herokuapp.com/api/images/random/pass';
+    request(flubrUrl, (error, response, body) => {
+      if (!error && response.statusCode === 200) {
+        const payload = {
           channel: req.channel,
           text: body,
-          icon_emoji: ':thumbsup:'
+          icon_emoji: ':thumbsup:',
         };
 
-        requestOpts = {
+        const requestOpts = {
           url: app.get('webhook'),
-          form: {payload: JSON.stringify(payload)}
+          form: {payload: JSON.stringify(payload)},
         };
 
-        request.post(requestOpts, function (err, resp, body) {
+        request.post(requestOpts, (err) => {
           if (err) {
             return res.status(500).send({success: false, error: err.message});
           }
           return res.status(200).send();
         });
-
       }
     });
-
   });
-
 };
