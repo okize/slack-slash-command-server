fa6bfe2caad40f11d66d2ea6622410d94d513272
Morgan Wigmanich <mwigmanich@patientslikeme.com>
2015-12-08 07:00:00 -0400

    fix eslint errors & warnings

--- routes/fail.js
+++ routes/fail.js
@@ -1,37 +1,28 @@
-var request = require('request');
+const request = require('request');
 
-module.exports = function (app) {
-
-  app.post('/fail', function (req, res) {
-
-    var flubrUrl = 'http://flubr.herokuapp.com/api/images/random/fail';
-
-    request(flubrUrl, function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-
-        var payload, requestOpts;
-
-        payload = {
+module.exports = (app) => {
+  app.post('/fail', (req, res) => {
+    const flubrUrl = 'http://flubr.herokuapp.com/api/images/random/fail';
+    request(flubrUrl, (error, response, body) => {
+      if (!error && response.statusCode === 200) {
+        const payload = {
           channel: req.channel,
           text: body,
-          icon_emoji: ':thumbsdown:'
+          icon_emoji: ':thumbsdown:',
         };
 
-        requestOpts = {
+        const requestOpts = {
           url: app.get('webhook'),
-          form: {payload: JSON.stringify(payload)}
+          form: {payload: JSON.stringify(payload)},
         };
 
-        request.post(requestOpts, function (err, resp, body) {
+        request.post(requestOpts, (err) => {
           if (err) {
             return res.status(500).send({success: false, error: err.message});
           }
           return res.status(200).send();
         });
-
       }
     });
-
   });
-
 };
