c245a68c4c83ac90735f81b74401759c774a7548
Morgan Wigmanich <mwigmanich@patientslikeme.com>
2016-01-06 07:00:00 -0400

    rename win to pass for consistency

--- routes/pass.js
+++ routes/pass.js
@@ -2,7 +2,7 @@ const request = require('request');
 const response = require('../lib/responseHelper');
 
 module.exports = (app) => {
-  app.post('/win', (req, res) => {
+  app.post('/pass', (req, res) => {
     const flubrUrl = 'http://flubr.herokuapp.com/api/images/random/pass';
     request(flubrUrl, (error, resp, body) => {
       if (!error && resp.statusCode === 200) {
