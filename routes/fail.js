a985fcf67f5046a02fc1f23c642d9f02c1746699
Morgan Wigmanich <mwigmanich@patientslikeme.com>
2016-01-06 07:00:00 -0400

    move last two scripts

--- routes/fail.js
+++ routes/fail.js
@@ -1,27 +1,18 @@
 const request = require('request');
+const response = require('../lib/responseHelper');
 
 module.exports = (app) => {
   app.post('/fail', (req, res) => {
     const flubrUrl = 'http://flubr.herokuapp.com/api/images/random/fail';
-    request(flubrUrl, (error, response, body) => {
-      if (!error && response.statusCode === 200) {
+    request(flubrUrl, (error, resp, body) => {
+      if (!error && resp.statusCode === 200) {
         const payload = {
           channel: req.channel,
           text: body,
           icon_emoji: ':thumbsdown:',
         };
 
-        const requestOpts = {
-          url: app.get('webhook'),
-          form: {payload: JSON.stringify(payload)},
-        };
-
-        request.post(requestOpts, (err) => {
-          if (err) {
-            return res.status(500).send({success: false, error: err.message});
-          }
-          return res.status(200).send();
-        });
+        response(payload, app, res);
       }
     });
   });
