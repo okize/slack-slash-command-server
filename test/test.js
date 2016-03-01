d505dda7b759454f55c5e5ffb9f0600d1dc3fdd5
Morgan Wigmanich <mwigmanich@patientslikeme.com>
2016-03-01 07:00:00 -0400

    fix tests such that server does not need to be running externally

--- test/test.js
+++ test/test.js
@@ -1,21 +1,19 @@
 const should = require('chai').should();
 const expect = require('chai').expect();
 const supertest = require('supertest');
-const api = supertest('http://localhost:9000');
+const app = supertest(require('../app.js'));
 
 describe('Web application', () => {
 
   it('should return a 200 response for the home page', (done) => {
-    api
+    app
       .get('/')
-      .set('Accept', 'text/html')
       .expect(200, done);
   });
 
   it('should return a 404 response for an incorrect page', (done) => {
-    api
+    app
       .get('/no-such-page')
-      .set('Accept', 'text/html')
       .expect(404, done);
   });
 
