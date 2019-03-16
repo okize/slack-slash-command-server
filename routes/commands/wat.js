77b37483dcd3c10584e7cc5408d97dd2038381d5
Morgan Wigmanich <mwigmanich@patientslikeme.com>
2019-03-16 07:00:00 -0400

    delete some commands

--- routes/commands/wat.js
+++
@@ -1,9 +0,0 @@
-module.exports = (req, cb) => {
-  const imageUrl = 'http://i.imgur.com/WlVVoGZ.gif';
-  const payload = {
-    icon_emoji: ':open_mouth:',
-    text: imageUrl,
-  };
-
-  return cb(payload);
-};
