f80d0dde0aca82d0270efe3d36b89e60fa5730a5
Morgan Wigmanich <mwigmanich@patientslikeme.com>
2019-03-30 07:00:00 -0400

    fix lint error

--- routes/command.js
+++ routes/command.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
-const sendToSlack = require('../lib/sendToSlack');
 const express = require('express');
+const sendToSlack = require('../lib/sendToSlack');
 
 const router = express.Router();
 
